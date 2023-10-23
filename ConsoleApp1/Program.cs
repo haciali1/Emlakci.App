137fd62fcc05f7f1670ae001d71b3396763b1073
HP <HP@DESKTOP-UM8PRBC>
2023-10-23 11:02:26 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Security.Principal;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var evim = new Ev();
+
+
+            Console.WriteLine("Oda sayısını giriniz : ");
+            evim.SetOdaSayisi(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Kat no giriniz : ");
+            evim.Katno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Alan giriniz : ");
+            evim.Alan = int.Parse(Console.ReadLine());
+            Console.WriteLine("Semt giriniz : ");
+            evim.Semt = Console.ReadLine();
+            Console.WriteLine("===================");
+            Console.WriteLine("Ev bilgileri");
+            Console.WriteLine(evim.EvBilgileri());
+        }
+
+
+     
+    }
+
+    class Ev
+    {
+        private int odasayisi;
+        private int katno;
+        private string semt;
+        private string buyukluk;
+
+        private double alan;
+
+        public double Alan
+        {
+            get
+            { return alan; }
+
+            set
+            {
+                alan = value;
+
+                if (alan <= 70)
+                {
+                    buyukluk = "Küçük Ev";
+                }
+                else if (alan > 70 && alan < 120)
+                {
+                    buyukluk = "Orta Ev";
+                }
+                else
+                {
+                    buyukluk = "Büyük ev";
+                }
+
+
+
+            }
+        }
+
+
+        public int Katno { get => katno; set => katno = value; }
+        public string Semt { get => semt; set => semt = value.ToUpper(); }
+        public string Buyukluk { get => buyukluk; }
+
+        public void SetOdaSayisi(int odasayi) => odasayisi = Math.Abs(odasayi);
+
+        public int GetOdaSayisi() => odasayisi;
+
+        public string EvBilgileri() => $"Alan:{alan}\nKat No: {katno}\nOda Sayısı: {odasayisi}\nSemt: {Semt}\nBüyüklük :{buyukluk}";
+
+    }
+}
---
+++ PersonelApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PersonelApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Personel();
+            Console.WriteLine("Ad Giriniz : ");
+            p.Ad=Console.ReadLine();
+            Console.WriteLine("Soyad Giriniz : ");
+            p.Soyad=Console.ReadLine();
+            Console.WriteLine("İşe giriş yılını giriniz : ");
+            p.GirisYil=int.Parse(Console.ReadLine());
+            Console.WriteLine("Personel Bilgileri :");
+            Console.WriteLine(p.PersonelBiilgi());
+        }
+    }
+    class Personel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        private double maas;
+        public double Maas { get => maas;  }
+      
+        private int girisyil;
+
+        public int GirisYil
+        {
+            get { return girisyil; }
+
+            set 
+            {
+                girisyil = value;
+
+               maas = 10000 + ((DateTime.Now.Year - girisyil)*1000);
+
+            }
+        }
+        public string PersonelBiilgi()=> $"Personel Adı :{Ad}\nPersonel Soyadı :{Soyad}\nPersonel Maaşı : {Maas}";
+      
+
+
+    }
+
+
+
+
+}
+// Personelin maaşı işe giriş yılına göre hesaplanacak
+//Maas=10000+(calistigiyil*1000);
